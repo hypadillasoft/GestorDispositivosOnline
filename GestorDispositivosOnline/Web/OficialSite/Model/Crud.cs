29f4c85eec8ca747ad0ce22ed3755a4bafe86690
steven4389 <40493520+steven4389@users.noreply.github.com>
2018-07-18 18:28:56 -0500

    oficialSite
    en la carpeta oficialSite que monte en la parte de web esta el frontend

---
+++ GestorDispositivosOnline/Web/OficialSite/Model/Crud.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Model
+{
+    class Crud
+    {
+
+        public ResponseModel Acceder(string Email, string Password)
+        {
+            var rm = new ResponseModel();
+
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    Password = HashHelper.MD5(Password);
+
+                    var usuario = ctx.Usuario.Where(x => x.Email == Email)
+                                             .Where(x => x.Password == Password)
+                                             .SingleOrDefault();
+
+                    if (usuario != null)
+                    {
+                        SessionHelper.AddUserToSession(usuario.id.ToString());
+                        rm.SetResponse(true);
+                    }
+                    else
+                    {
+                        rm.SetResponse(false, "Correo o contraseña incorrecta");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return rm;
+        }
+
+        public Usuario Obtener(int id, bool includes = false)
+        {
+            var usuario = new Usuario();
+
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    if (!includes)
+                    {
+                        usuario = ctx.Usuario.Where(x => x.id == id)
+                                             .SingleOrDefault();
+                    }
+                    else
+                    {
+                        usuario = ctx.Usuario.Include("Experiencia")
+                                             .Include("Habilidad")
+                                             .Include("Testimonio")
+                                             .Where(x => x.id == id)
+                                             .SingleOrDefault();
+                    }
+
+                    // Trayendo un registro adicional de manera manual, sin usar Include
+                    usuario.Pais = new TablaDato().Obtener("pais", usuario.Pais_id.ToString());
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return usuario;
+        }
+
+        public ResponseModel Guardar(HttpPostedFileBase Foto)
+        {
+            var rm = new ResponseModel();
+
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    ctx.Configuration.ValidateOnSaveEnabled = false;
+
+                    var eUsuario = ctx.Entry(this);
+                    eUsuario.State = EntityState.Modified;
+
+                    // Campos que queremos ignorar
+                    if (Foto != null)
+                    {
+                        // Nombre del archivo, es decir, lo renombramos para que no se repita nunca
+                        string archivo = DateTime.Now.ToString("yyyyMMddHHmmss") + Path.GetExtension(Foto.FileName);
+
+                        // La ruta donde lo vamos guardar
+                        Foto.SaveAs(HttpContext.Current.Server.MapPath("~/uploads/" + archivo));
+
+                        // Establecemos en nuestro modelo el nombre del archivo
+                        this.Foto = archivo;
+                    }
+                    else eUsuario.Property(x => x.Foto).IsModified = false;
+
+                    if (this.Password == null) eUsuario.Property(x => x.Password).IsModified = false;
+
+                    ctx.SaveChanges();
+
+                    rm.SetResponse(true);
+                }
+            }
+            catch (DbEntityValidationException e)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return rm;
+        }
+    }
+}
+
+    }
+}
---
+++ GestorDispositivosOnline/Web/OficialSite/Repositorio/ResponseModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ResponseModel
+    {
+	    public dynamic Result{get;set;}
+	    public bool Response{get;set;}
+	    public string Message{get;set;}
+	    public string Href {get;set;}
+	    public string Function {get;set;}
+
+        public ResponseModel() 
+        {
+            this.Response = false;
+            this.Message = "Ocurrio un error inesperado";
+        }
+	
+	    public void SetResponse(bool r, string m = "")
+	    {
+		    this.Response = r;
+		    this.Message = m;
+
+		    if(!r && m == "") this.Message = "Ocurrio un error inesperado";
+	    }
+    }
+}
